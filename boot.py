7f4aae077d8e8121f7dd8a8972f8971d36927df1
imninety9 <72973422+imninety9@users.noreply.github.com>
2024-12-22 23:48:42 +0530

    Add files via upload

---
+++ boot.py
@@ -0,0 +1,9 @@
+# boot.py
+
+from time import sleep
+
+print("Sleeping for 5 sec to let router connect to wifi in case of power cut.")
+
+sleep(5)
+    
+print("Exiting boot.py; starting with the main program...")
